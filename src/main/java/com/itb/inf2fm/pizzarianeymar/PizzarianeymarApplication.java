8ada0a50839a99878460c7f8abaab73dc7b0e87d
Soares10-a <rm99642@estudante.fieb.edu.br>
2026-05-28 11:00:51 +0000

    mensagem start servidor toncat

--- src/main/java/com/itb/inf2fm/pizzarianeymar/PizzarianeymarApplication.java
+++ src/main/java/com/itb/inf2fm/pizzarianeymar/PizzarianeymarApplication.java
@@ -8,6 +8,8 @@ public class PizzarianeymarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzarianeymarApplication.class, args);
+		System.out.println("Servidor rodando na porta 8080")
+
 	}
 
 }
